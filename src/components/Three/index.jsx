1e42a51cab8b8a110d2efd86d1db874dfbfcd0ca
Talha <73413298+TalhaAmjad0034@users.noreply.github.com>
2023-02-19 17:06:40 +0500

    get the orbit controls of camera

--- src/components/Three/index.jsx
+++ src/components/Three/index.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { angleToRadians } from "../../utils/angle";
+import { useFrame } from "@react-three/fiber";
 
 function index() {
+  const orbitControlsRef = useRef();
+
+  useFrame(() => {
+    orbitControlsRef.current.update();
+  });
+
+  useEffect(() => {
+    if (!!orbitControlsRef.current) {
+      console.log(orbitControlsRef.current);
+    }
+  }, []);
+
   return (
     <>
       {/* Camera */}
       <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-      <OrbitControls />
+      <OrbitControls ref={orbitControlsRef} />
       {/* Ball */}
       <mesh position={[0, 1, 0]}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="red" />
       </mesh>
 
+      {/* Adding plane */}
       <mesh rotation={[-angleToRadians(45), 0, 0]}>
-        {/* Adding plane */}
         <planeGeometry args={[7, 7]} />
         <meshStandardMaterial color="green" />
       </mesh>
